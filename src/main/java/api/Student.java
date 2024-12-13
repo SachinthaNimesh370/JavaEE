2c9b475af15c4d93c0a4fdedfcb5865665c3e7c3
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-12-13 20:47:34 +0530

    Create basic Structure

---
+++ src/main/java/api/Student.java
@@ -0,0 +1,15 @@
+package api;
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Student extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
